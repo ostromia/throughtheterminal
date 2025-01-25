4d6547294cfa07e12d0c78ab6d48e57337ed9c65
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2025-01-25 22:44:39 +0000

    getting file path from command line arguments

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use std::env;
+
+use std::thread;
+use std::time::Duration;
+
+fn windows(file_path: String) {
+    println!("Windows machine detected\n");
+    println!("file path: {}", file_path);
+    thread::sleep(Duration::new(10, 0));
+}
+
+fn macos(file_path: String) {
+    println!("MacOS machine detected\n");
+    println!("file path: {}", file_path);
+    thread::sleep(Duration::new(10, 0));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let platform = env::consts::OS;
+    let arguments: Vec<String> = env::args().collect();
+
+    if arguments.len() >= 2 {
+        let file_path = arguments[1].clone();
+
+        if platform == "windows" {
+            windows(file_path);
+        }
+        else if platform == "macos" {
+            macos(file_path);
+        }
+    }
 }
