2c1362eb86e791fb8ba2bf361e054368b4999b75
ostromia <66202981+ostromia@users.noreply.github.com>
2025-01-27 02:56:01 +0000

    clean up

--- src/macos.rs
+++ src/macos.rs
@@ -1,16 +1,2 @@
-use std::process::Command;
-
 pub fn macos(file_path: String) {
-    let _ = Command::new("osascript")
-        .arg("-e")
-        .arg(format!(
-            r#"
-            tell application "Terminal"
-                activate
-                do script "nvim \"{}\""
-            end tell
-            "#,
-        file_path
-        ))
-        .output();
 }
--- src/windows.rs
+++ src/windows.rs
@@ -1,7 +1,2 @@
-use std::process::Command;
-
 pub fn windows(file_path: String) {
-    let _ = Command::new("wt")
-        .args(["--window", "0", "new-tab", "nvim", &file_path])
-        .output();
 }
