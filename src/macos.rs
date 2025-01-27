157097f2ae68c2ad881cb6a29f7ce4c2453d98dc
ostromia <66202981+ostromia@users.noreply.github.com>
2025-01-27 05:18:40 +0000

    implement for Terminal.app

--- src/macos.rs
+++ src/macos.rs
@@ -1,9 +1,10 @@
 use std::process::Command;
 
-fn terminalapp(editor: &String, file_path: &String) {
-    let _ = Command::new("osascript")
-        .arg("-e")
-        .arg(format!(
+fn terminalapp(editor: &String, method: &String, file_path: &String) {
+    let applescript: String;
+
+    if method == "tab" {
+        applescript = format!(
             r#"
             tell application "Terminal"
                 activate
@@ -12,12 +13,29 @@ fn terminalapp(editor: &String, file_path: &String) {
             "#,
             editor,
             file_path
-        ))
+        );
+    }
+    else {
+        applescript = format!(
+            r#"
+            tell application "Terminal"
+                do script "{} \"{}\""
+                activate
+            end tell
+            "#,
+            editor,
+            file_path
+        );
+    }
+
+    let _ = Command::new("osascript")
+        .arg("-e")
+        .arg(applescript)
         .output();
 }
 
 pub fn macos(terminal: &String, editor: &String, method: &String, file_path: String) {
     if terminal == "Terminal" {
-        terminalapp(editor, &file_path);
+        terminalapp(editor, method, &file_path);
     }
 }
