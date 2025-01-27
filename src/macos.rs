176c92a171456f7cc2a5fbe6ae41e078179c19d8
ostromia <66202981+ostromia@users.noreply.github.com>
2025-01-27 04:16:57 +0000

    feat: support for Terminal.app

--- src/macos.rs
+++ src/macos.rs
@@ -1,4 +1,23 @@
 use std::process::Command;
 
+fn terminalapp(editor: &String, file_path: &String) {
+    let _ = Command::new("osascript")
+        .arg("-e")
+        .arg(format!(
+            r#"
+            tell application "Terminal"
+                activate
+                do script "{} \"{}\""
+            end tell
+            "#,
+            editor,
+            file_path
+        ))
+        .output();
+}
+
 pub fn macos(terminal: &String, editor: &String, file_path: String) {
+    if terminal == "Terminal" {
+        terminalapp(editor, &file_path);
+    }
 }
