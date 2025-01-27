e44df95bcd0d09768d4bc0107db241a8773c1294
berkay-yalin <66202981+berkay-yalin@users.noreply.github.com>
2025-01-27 04:28:59 +0000

    feat: support for Windows Terminal

--- src/windows.rs
+++ src/windows.rs
@@ -1,4 +1,13 @@
 use std::process::Command;
 
+fn windows_terminal(editor: &String, file_path: &String) {
+    let _ = Command::new("wt")
+        .args(["--window", "0", "new-tab", editor, &file_path])
+        .output();
+}
+
 pub fn windows(terminal: &String, editor: &String, file_path: String) {
+    if terminal == "Windows Terminal" {
+        windows_terminal(editor, &file_path);
+    }
 }
