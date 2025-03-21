b9c25d33d18444d552c34399018ac65898866630
berkay yalin <66202981+ostromia@users.noreply.github.com>
2025-03-21 00:43:54 +0000

    feat: add support for Ghostty on macOS (#3)
    * support ghostty
    
    * update version

--- src/macos.rs
+++ src/macos.rs
@@ -1,5 +1,40 @@
 use std::process::Command;
 
+fn ghostty(editor: &String, method: &String, file_path: &String) {
+    let applescript = format!(
+        r#"
+            set isGhosttyRunning to false
+
+            if application "Ghostty" is running then
+                set isGhosttyRunning to true
+            end if
+
+            tell application "Ghostty"
+                activate
+            end tell
+
+            tell application "System Events"
+                keystroke "{}" using command down
+            end tell
+
+            delay 0.1
+
+            tell application "System Events"
+                keystroke "{} \"{}\""
+                key code 36
+            end tell
+        "#,
+        if method == "tab" { "t" } else { "n" },
+        editor,
+        file_path
+    );
+
+    let _ = Command::new("osascript")
+        .arg("-e")
+        .arg(applescript)
+        .output();
+}
+
 fn terminalapp(editor: &String, method: &String, file_path: &String) {
     let applescript: String;
 
@@ -37,5 +72,9 @@ fn terminalapp(editor: &String, method: &String, file_path: &String) {
 pub fn macos(terminal: &String, editor: &String, method: &String, file_path: String) {
     if terminal == "Terminal" {
         terminalapp(editor, method, &file_path);
+    }
+
+    else if terminal == "Ghostty" {
+        ghostty(editor, method, &file_path);
     }
 }
